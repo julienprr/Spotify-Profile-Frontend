44230466ff04c58156be4c58ef83beaebf004d3a
Julien Perrier <perrierjulien22@gmail.com>
2025-07-31 12:20:05 +0200

    Fix artist cover image

--- src/components/track/TrackList.tsx
+++ src/components/track/TrackList.tsx
@@ -1,7 +1,7 @@
-import type { TrackProps } from './Track';
+import defaultTrackCover from '@/assets/images/default_track_cover_small.jpg';
+import type { TTrack } from './Track';
 import Track from './Track';
 import TrackSkeleton from './TrackSkeleton';
-import defaultTrackCover from '@/assets/images/default_track_cover_small.jpg';
 
 const TrackList = ({ items, isLoading }: { items: any; isLoading: boolean }) => {
   if (isLoading) {
@@ -25,8 +25,8 @@ const TrackList = ({ items, isLoading }: { items: any; isLoading: boolean }) => 
     return (
       <>
         <div className="flex flex-col space-y-4 sm:space-y-6">
-          {items.map((item: TrackProps) => {
-            const track: TrackProps = {
+          {items.map((item: TTrack) => {
+            const track = {
               id: item.id,
               name: item.name,
               artistName: item.artistName || 'undefined',
@@ -36,7 +36,7 @@ const TrackList = ({ items, isLoading }: { items: any; isLoading: boolean }) => 
               duration: item.duration,
             };
 
-            return <Track key={track.id} track={track} />;
+            return <Track key={track.id} track={track} isLoading={isLoading} />;
           })}
         </div>
       </>
